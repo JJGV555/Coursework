3f98c710e64b7fa82d581d3b92408623e60b4588
JJGV555 <92362@farnborough.ac.uk>
2020-12-01 12:46:35 +0000

    working on javascript to display records as a table, table made before-hand put as a comment in the html file. current error: "formatDnameList is not defined"

--- resources/client/js/Drivers.js
+++ resources/client/js/Drivers.js
@@ -1,9 +1,23 @@
+"use strict";
 function searchdriver() {
     console.log("invoked searchdriver")
     var driver = document.getElementById('DriverSelect').value;
     if (driver == 'blank') {
         console.log("a driver must be selected to continue");
     } else {
+        const dname = driver;
+        const url = "/dinfo/list/";
 
+        fetch(url + dname, {
+            method: "GET",
+        }).then(response => {
+            return response.json();
+        }).then(response => {
+            if (response.hasOwnProperty("Error")) {
+                alert(JSON.stringify(response));
+            } else {
+                formatDnameList(response);
+            }
+        })
     }
 }
